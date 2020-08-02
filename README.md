c08abd53d876781651e4841c5632cfb1ac283771
MoAlyousef <may642_2000@hotmail.com>
2020-08-02 21:46:42 +0300

    Add example and doc entry

--- README.md
+++ README.md
@@ -3,5 +3,47 @@ Raw GLU and GL Rust bindings
 
 This crate doesn't handle windowing, it can be used with other crates which handle windowing and gl contexts to do raw opengl calls.
 
-This is an example using the fltk crate:
-https://github.com/MoAlyousef/fltk-opengl-demo/blob/master/src/main.rs
+```rust
+fn draw_triangle() {
+    use glu_sys::*;
+    unsafe {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glViewport(0, 0, W, H);
+        gluPerspective(45.0, (W as f32 / H as f32).into(), 1.0, 10.0);
+        glTranslatef(0.0, 0.0, -5.0);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        glRotatef(0.0, 1.0, 1.0, 0.0);
+        glColor3f(1.0, 0.0, 0.0);
+        glBegin(GL_POLYGON);
+        glVertex3f(0.0, 1.0, 0.0);
+        glVertex3f(1.0, -1.0, 1.0);
+        glVertex3f(-1.0, -1.0, 1.0);
+        glEnd();
+        glColor3f(0.0, 1.0, 0.0);
+        glBegin(GL_POLYGON);
+        glVertex3f(0.0, 1.0, 0.0);
+        glVertex3f(0.0, -1.0, -1.0);
+        glVertex3f(1.0, -1.0, 1.0);
+        glEnd();
+        glColor3f(0.0, 0.0, 1.0);
+        glBegin(GL_POLYGON);
+        glVertex3f(0.0, 1.0, 0.0);
+        glVertex3f(-1.0, -1.0, 1.0);
+        glVertex3f(0.0, -1.0, -1.0);
+        glEnd();
+        glColor3f(1.0, 0.0, 0.0);
+        glBegin(GL_POLYGON);
+        glVertex3f(1.0, -1.0, 1.0);
+        glVertex3f(0.0, -1.0, -1.0);
+        glVertex3f(-1.0, -1.0, 1.0);
+        glEnd();
+        glLoadIdentity();
+        glRasterPos2f(-3.0, -2.0);
+    }
+}
+```
+
+Full example using the fltk crate [here](https://github.com/MoAlyousef/fltk-rs-demos/blob/master/opengl/src/main.rs).
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,53 @@
+//! # glu-sys
+//! Raw GLU and GL Rust bindings
+
+//! This crate doesn't handle windowing, it can be used with other crates which handle windowing and gl contexts to do raw opengl calls.
+
+//! ```rust
+//! fn draw_triangle() {
+//!     use glu_sys::*;
+//!     unsafe {
+//!         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+//!         glMatrixMode(GL_PROJECTION);
+//!         glLoadIdentity();
+//!         glViewport(0, 0, W, H);
+//!         gluPerspective(45.0, (W as f32 / H as f32).into(), 1.0, 10.0);
+//!         glTranslatef(0.0, 0.0, -5.0);
+//!         glMatrixMode(GL_MODELVIEW);
+//!         glLoadIdentity();
+//!         glRotatef(0.0, 1.0, 1.0, 0.0);
+//!         glColor3f(1.0, 0.0, 0.0);
+//!         glBegin(GL_POLYGON);
+//!         glVertex3f(0.0, 1.0, 0.0);
+//!         glVertex3f(1.0, -1.0, 1.0);
+//!         glVertex3f(-1.0, -1.0, 1.0);
+//!         glEnd();
+//!         glColor3f(0.0, 1.0, 0.0);
+//!         glBegin(GL_POLYGON);
+//!         glVertex3f(0.0, 1.0, 0.0);
+//!         glVertex3f(0.0, -1.0, -1.0);
+//!         glVertex3f(1.0, -1.0, 1.0);
+//!         glEnd();
+//!         glColor3f(0.0, 0.0, 1.0);
+//!         glBegin(GL_POLYGON);
+//!         glVertex3f(0.0, 1.0, 0.0);
+//!         glVertex3f(-1.0, -1.0, 1.0);
+//!         glVertex3f(0.0, -1.0, -1.0);
+//!         glEnd();
+//!         glColor3f(1.0, 0.0, 0.0);
+//!         glBegin(GL_POLYGON);
+//!         glVertex3f(1.0, -1.0, 1.0);
+//!         glVertex3f(0.0, -1.0, -1.0);
+//!         glVertex3f(-1.0, -1.0, 1.0);
+//!         glEnd();
+//!         glLoadIdentity();
+//!         glRasterPos2f(-3.0, -2.0);
+//!     }
+//! }
+//! ```
+
+//! Full example using the fltk crate [here](https://github.com/MoAlyousef/fltk-rs-demos/blob/master/opengl/src/main.rs).
+
 #![no_std]
 #![allow(non_camel_case_types)]
 #![allow(dead_code)]
